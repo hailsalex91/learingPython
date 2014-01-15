d359f8fb4743c86737a8a96694c5c64b19eb063e
hailsalex91 <hails.alex91@gmail.com>
2014-01-15 13:29:17 -0500

    writing my first helloWorld

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+# learning python commands
+print "hello world"
+print "my name is Seven"
+print "Lets count some things"
+
+print "Boys", 25+30/6
+print "Girls", 100-25*3%4
+
+print "Let's count some things"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
