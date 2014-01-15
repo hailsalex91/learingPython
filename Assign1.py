eb8fd2a788a383e8ebcc3e53837e1923b8a5b886
hailsalex91 <hails.alex91@gmail.com>
2014-01-15 14:19:24 -0500

    first assignment

---
+++ Assign1.py
@@ -0,0 +1,13 @@
+name = "Alex Hails"
+age = 22
+in_school = 4
+brother = 20
+food = "sushi"
+
+print "Helloworld! My name is", name, "and I am %d years old!" % age
+print "My younger brother is %d years old." % brother
+print "Together, we have lived for", age+brother, "years."
+print "I have been in school for", in_school, "years."
+print "If I had 2 kids, and they went to school as long as me, they will be in school for", in_school*2, "years."
+print "I have been in school for " +str(in_school/float(age)*100)+ "% of my life."
+print "My favorite food is %s." % food
